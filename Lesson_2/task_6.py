a240afe8b3a162ee0a74f3e5c250d32e97709ffb
AlexProsku <pros1217@yandex.ru>
2020-12-09 03:50:05 +0300

    correction pep - task_6

--- Lesson_2/task_6.py
+++ Lesson_2/task_6.py
@@ -15,14 +15,14 @@ products_list = []
 numb_item = 1
 while True:
     products_item = list(str(input("Введите через пробел: "
-                               "\nназвание товара, цену, количество, единицу измерения (шт, кг, м) "
-                               "\nи нажмите Enter (ввод). "
-                               "\nДля завершения ввода товаров - введите END и/или нажмите Enter : ")
-                         ).split())
+                                   "\nназвание товара, цену, количество, единицу измерения (шт, кг, м) "
+                                   "\nи нажмите Enter (ввод). "
+                                   "\nДля завершения ввода товаров - введите END и/или нажмите Enter : ")
+                             ).split())
     if products_item in [["END"], ["end"], []]:
         break
     products_dict = dict(НазваниеТовара=products_item[0], Цена=products_item[1], Количество=products_item[2],
-                   ЕдиницаИзмерения=products_item[3])
+                         ЕдиницаИзмерения=products_item[3])
     products_tuple = numb_item, products_dict
     products_list.append(products_tuple)
     numb_item += 1
@@ -37,6 +37,6 @@ for i in products_list:
 analitics = dict(НазваниеТовара=(list(set(products_name))), Цена=(list(set(price))),
                  Количество=(list(set(quantity))), ЕдиницаИзмерения=(list(set(measure))))
 print("Аналитика:")
-print(str(analitics).replace('], ','],\n'))     # с f строкой не придумал как сделать, что отображалось как в примере.
+print(str(analitics).replace('], ', '],\n'))     # с f строкой не придумал как сделать, что отображалось как в примере.
 # print(*analitics, sep='\n')
 # В последнем закоменченном print съедаются значения из словаря - подозреваю из-за разных типов данных (строка и список)
